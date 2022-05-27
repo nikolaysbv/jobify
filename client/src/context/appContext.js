f1bebe045b0e004bfe3364dc2e62b54aecb8824d
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-05-27 13:53:54 +0300

    added alert for leaving empty fields in register/login

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,6 @@
 import React, { useReducer, useContext } from "react"
 import reducer from "./reducer"
+import { CLEAR_ALERT, DISPLAY_ALERT } from "./actions"
 
 const initialState = {
   isLoading: false,
@@ -13,8 +14,21 @@ const AppContext = React.createContext()
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState)
 
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT })
+    clearAlert()
+  }
+
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({ type: CLEAR_ALERT })
+    }, 3000)
+  }
+
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, displayAlert }}>
+      {children}
+    </AppContext.Provider>
   )
 }
 
