bcb4ba9fe4559a64032bcf4a5183f72cfda2e322
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-05-27 16:24:47 +0300

    Revert "Revert "created User model""
    This reverts commit 9ef80c18b5291b3a4ccea174e0f35e489050a335.

---
+++ controllers/authController.js
@@ -0,0 +1,11 @@
+const register = async (req, res) => {
+  res.send("register")
+}
+const login = async (req, res) => {
+  res.send("login")
+}
+const updateUser = async (req, res) => {
+  res.send("update user")
+}
+
+export { register, login, updateUser }
