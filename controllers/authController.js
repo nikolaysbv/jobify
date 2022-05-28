c1054fa1caee99bd5eeef1027b97ccf0dd1855df
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-05-28 17:17:48 +0300

    set up structure for errors in controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,9 @@
-const register = async (req, res) => {
-  res.send("register")
+import { StatusCodes } from "http-status-codes"
+import User from "../models/User.js"
+
+const register = async (req, res, next) => {
+  const user = await User.create(req.body)
+  res.status(StatusCodes.CREATED).json({ user })
 }
 const login = async (req, res) => {
   res.send("login")
