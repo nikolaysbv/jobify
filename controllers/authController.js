0ee687633c5e9583a1d4fc802d14b2431474ab00
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-05-30 12:01:41 +0300

    added hashing password before sending to db

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
 import { StatusCodes } from "http-status-codes"
 import User from "../models/User.js"
+import { BadRequestError, NotFoundError } from "../errors/index.js"
 
 const register = async (req, res, next) => {
   const { name, email, password } = req.body
 
   if (!name || !email || !password) {
-    throw new Error("please provide all values")
+    throw new BadRequestError("please provide all values")
+  }
+
+  const userAlreadyExists = await User.findOne({ email })
+  if (userAlreadyExists) {
+    throw new BadRequestError("Email already in use")
   }
 
   const user = await User.create({ name, email, password })
