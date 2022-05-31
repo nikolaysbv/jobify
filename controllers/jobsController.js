485b4447fe5db394562aa46584c27661f703e93a
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-05-31 21:24:00 +0300

    created update job functionality on server

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,6 +1,7 @@
 import Job from "../models/Job.js"
 import { StatusCodes } from "http-status-codes"
 import { BadRequestError, NotFoundError } from "../errors/index.js"
+import checkPermissions from "../utils/checkPermissions.js"
 
 const createJob = async (req, res) => {
   const { position, company } = req.body
@@ -23,7 +24,27 @@ const getAllJobs = async (req, res) => {
 }
 
 const updateJob = async (req, res) => {
-  res.send("update job")
+  const { id: jobId } = req.params
+  const { company, position } = req.body
+
+  if (!company || !position) {
+    throw new BadRequestError("Please provide all values!")
+  }
+
+  const job = await Job.findOne({ _id: jobId })
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`)
+  }
+
+  checkPermissions(req.user, job.createdBy)
+
+  const updatedJob = await Job.findOneAndUpdate({ _id: jobId }, req.body, {
+    new: true,
+    runValidators: true,
+  })
+
+  res.status(StatusCodes.OK).json({ updatedJob })
 }
 
 const deleteJob = async (req, res) => {
