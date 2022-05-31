4baafad85de3723293313209883a07f43ad3dd61
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-05-31 20:54:03 +0300

    set up a function which changes the state to the job edited when editing is started

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -19,6 +19,7 @@ import {
   CREATE_JOB_ERROR,
   GET_JOBS_BEGIN,
   GET_JOBS_SUCCESS,
+  SET_EDIT_JOB,
 } from "./actions"
 
 const token = localStorage.getItem("token")
@@ -216,7 +217,11 @@ const AppProvider = ({ children }) => {
   }
 
   const setEditJob = (id) => {
-    console.log(`set edit job : ${id}`)
+    dispatch({ type: SET_EDIT_JOB, payload: { id } })
+  }
+
+  const editJob = () => {
+    console.log("edit job")
   }
 
   const deleteJob = (id) => {
@@ -238,6 +243,7 @@ const AppProvider = ({ children }) => {
         getJobs,
         setEditJob,
         deleteJob,
+        editJob,
       }}
     >
       {children}
