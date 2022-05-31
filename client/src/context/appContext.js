a5597b240783b19ab0fb841a9246299d6d374955
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-05-31 22:01:43 +0300

    created edit job functionality in frontend

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -21,6 +21,9 @@ import {
   GET_JOBS_SUCCESS,
   SET_EDIT_JOB,
   DELETE_JOB_BEGIN,
+  EDIT_JOB_BEGIN,
+  EDIT_JOB_SUCCESS,
+  EDIT_JOB_ERROR,
 } from "./actions"
 
 const token = localStorage.getItem("token")
@@ -221,8 +224,27 @@ const AppProvider = ({ children }) => {
     dispatch({ type: SET_EDIT_JOB, payload: { id } })
   }
 
-  const editJob = () => {
-    console.log("edit job")
+  const editJob = async () => {
+    dispatch({ type: EDIT_JOB_BEGIN })
+    try {
+      const { position, company, jobLocation, jobType, status } = state
+      await authFetch.patch(`/jobs/${state.editJobId}`, {
+        company,
+        position,
+        jobLocation,
+        jobType,
+        status,
+      })
+      dispatch({ type: EDIT_JOB_SUCCESS })
+      clearValues()
+    } catch (error) {
+      if (error.response.status === 401) return
+      dispatch({
+        type: EDIT_JOB_ERROR,
+        payload: { msg: error.response.data.msg },
+      })
+    }
+    clearAlert()
   }
 
   const deleteJob = async (jobId) => {
