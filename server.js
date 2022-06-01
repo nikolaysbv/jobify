bf1ee13690d6f71cf6552ccdc95d53c003948071
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-06-01 13:54:35 +0300

    set up security

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ dotenv.config()
 import "express-async-errors"
 import morgan from "morgan"
 
+// necessary in order to serve static assets
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import path from "path"
+
+// security packages
+import helmet from "helmet"
+import xss from "xss-clean"
+import mongoSanitize from "express-mongo-sanitize"
+
 // db and authenticateUser
 import connectDB from "./db/connect.js"
 
@@ -23,14 +33,20 @@ if (process.env.NODE_ENV !== "production") {
   app.use(morgan("dev"))
 }
 
-app.use(express.json())
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-app.get("/", (req, res) => {
-  res.send("welcome")
-})
+app.use(express.json())
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
+app.use(express.static(path.resolve(__dirname, "./client/build")))
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/jobs", authenticateUser, jobsRouter)
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"))
+})
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
